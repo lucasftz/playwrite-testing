f09b95f624b7fe27dc800fc3f4b018bd0fb2b451
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2023-03-16 15:17:39 -0400

    fix one thing but not the other

--- e2e/spec.ts
+++ e2e/spec.ts
@@ -1,11 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-test("redirect to login", async ({ page }) => {
+test("login", async ({ page }) => {
   await page.goto(process.env.WEBSITE_URL);
   await expect(page.getByText("Login page")).toBeVisible();
-});
-
-test("login", async ({ page }) => {
   await page.getByPlaceholder("username").fill("test user");
   await expect(page.getByText("Hello, test user"));
 });
