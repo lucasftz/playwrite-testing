3d0ae0e76d36948d9be650dda0fa1d69d42b1dc1
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2023-03-16 15:09:11 -0400

    make wrong test

--- e2e/spec.ts
+++ e2e/spec.ts
@@ -4,3 +4,8 @@ test("redirect to login", async ({ page }) => {
   await page.goto(process.env.WEBSITE_URL);
   await expect(page.getByText("Login page")).toBeVisible();
 });
+
+test("login", async ({ page }) => {
+  await page.getByPlaceholder("username").fill("test user");
+  await expect(page.getByText("Hello, test user"));
+});
