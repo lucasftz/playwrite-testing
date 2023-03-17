7d1aea15633a2979e97572d42071ee61ed185e61
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2023-03-16 20:30:55 -0400

    change import path

--- e2e/spec.ts
+++ e2e/spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { config } from "@/config";
+import { config } from "../config";
 
 test("login", async ({ page }) => {
   await page.goto(config.WEBSITE_URL);
